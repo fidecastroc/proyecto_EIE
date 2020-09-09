7fc3f9e3c363dcd22eeddfb0a67e3975a162e1b8
FidelC <fidel.castroc@ieee.org>
2020-09-08 18:13:07 -0600

    este funciona

--- untitled0.py
+++ untitled0.py
@@ -1,15 +1,38 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import odeint
 from scipy import integrate, optimize
 
 ydata = [3, 8, 26, 76, 225, 298, 258, 233, 189, 128, 68, 29, 14, 4]
-xdata = np.linspace(0,14, dtype="float")
+xdata = list(range(0, 14))
 
 
 
 ydata = np.array(ydata, dtype=float)
 xdata = np.array(xdata, dtype=float)
+
+def derivSt_SIR( y, t, beta, gamma):
+    S,  I, R = y
+    dSdt = -beta * S * I
+    dIdt=  beta * S * I - gamma * I 
+    dRdt = gamma * I
+    return  dSdt, dIdt, dRdt
+
+
+def SIR(S0, I0, beta, gamma, days):
+    y0 = S0, I0, 0
+    t = list(range(0, days))
+    result = odeint(derivSt_SIR, y0, t, args=(beta, gamma))
+    S, I, R = result.T
+    plt.plot(S)
+    plt.plot(I)
+    plt.plot(R)
+    plt.grid()
+    plt.legend(['s',"i",'r'])
+    plt.title("Modelo SIR")
+    plt.show()
+    return 0
 
 def sir_model(y, x, beta, gamma):
     S = -beta * y[0] * y[1] / N
@@ -20,14 +43,19 @@ def sir_model(y, x, beta, gamma):
 def fit_odeint(x, beta, gamma):
     return integrate.odeint(sir_model, (S0, I0, R0), x, args=(beta, gamma))[:,1]
 
-N = sum(ydata)
+N = 763
 I0 = ydata[0]
 S0 = N - I0
 R0 = 0.0
 
 popt, pcov = optimize.curve_fit(fit_odeint, xdata, ydata)
 fitted = fit_odeint(xdata, *popt)
+beta1, gamma1 = popt
+print(beta1,gamma1)
+
+SIR(S0, I0,  beta1/N, gamma1,14)
+
 
 plt.plot(xdata, ydata, 'o')
-plt.plot(xdata, fitted)
+#lt.plot(xdata, fitted)
 plt.show()
--- untitled1.py
+++ untitled1.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 ydata = [3, 8, 26, 76, 225, 298, 258, 233, 189, 128, 68, 29, 14, 4]
 xdata = np.linspace(0,14)
-yhat = savgol_filter(ydata, 13, 11)
+yhat = savgol_filter(ydata, 7, 6)
 
 
 
