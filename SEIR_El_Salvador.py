a52123db24bdbdd53a1d4147f163c4aebb17209a
FidelC <fidel.castroc@ieee.org>
2020-10-11 15:02:52 -0600

    datoos el salvador

--- SEIR_El_Salvador.py
+++ SEIR_El_Salvador.py
@@ -10,9 +10,13 @@ datos = pd.read_csv('El Salvadr_Activos.csv')
 df=pd.DataFrame(datos)
 nmp=df.to_numpy()
 activos=(nmp[3:4])
+recuperados= (nmp[1:2])
 j , dias = activos.shape 
 ejex = list(range(0, dias))
 activos = activos[0]
+
+recuperados=recuperados[0]
+
 #activos = np.array(activos, dtype=float)
 #ejex = np.array(ejex, dtype=float)
 #plt.plot(activos)
@@ -30,9 +34,9 @@ def SIR(S0, I0, beta, gamma, days):
     t = list(range(0, days))
     result = odeint(derivSt_SIR, y0, t, args=(beta, gamma))
     S, I, R = result.T
-    #plt.plot(S)
+    plt.plot(S)
     plt.plot(I)
-    #plt.plot(R)
+    plt.plot(R)
     plt.grid()
     plt.legend(['s',"i",'r'])
     plt.title("Modelo SIR")
@@ -49,25 +53,28 @@ def fit_odeint(x, beta, gamma):
     return integrate.odeint(sir_model, (S0, I0, R0), x, args=(beta, gamma))[:,1]
 
 
-#ydata = [3, 8, 26, 76, 225, 298, 258, 233, 189, 128, 68, 29, 14, 4]
-#xdata = list(range(0, 14))
+ydata = [3, 8, 26, 76, 225, 298, 258, 233, 189, 128, 68, 29, 14, 4]
+xdata = list(range(0, 14))
 
-ydata= activos
-xdata= ejex
+#ydata= activos
+#ydata1=recuperados
+#xdata= ejex
 
-ydata = np.array(ydata, dtype=float)
-xdata = np.array(xdata, dtype=float)
+#ydata = np.array(ydata, dtype=float)
+#ydata1= np.array(ydata1, dtype=float)
+#xdata = np.array(xdata, dtype=float)
 
-N = 6000000
+N = 763
 I0 = ydata[0]
 S0 = N - I0
 R0 = 0
 
 popt, pcov = optimize.curve_fit(fit_odeint, xdata, ydata)
+#pop , pcov = optimize.curve_fit(fit_odeint, xdata, ydata1)
 fitted = fit_odeint(xdata, *popt)
+#fitted1= fit_odeint(xdata, *pop)
 beta1, gamma1 = popt
-SIR(S0, I0,  beta1/N, gamma1, 400)
 
-plt.plot(xdata, ydata, 'o')
-plt.plot(xdata, fitted)
-#plt.show()
+SIR(S0, I0,  beta1/N, gamma1, 300)
+
+
